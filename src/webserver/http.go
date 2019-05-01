b9ff4fa3d2223e42426e46f286bf64ae24112dbe
Eric Bardes <ericbardes@gmail.com>
2019-05-01 01:03:00 -0400

    HTML and style updates

--- src/webserver/http.go
+++ src/webserver/http.go
@@ -2,9 +2,11 @@ package webserver
 
 import (
 	"fmt"
-	"log"
+	"net"
 	"net/http"
 	"v2/config"
+
+	"github.com/rs/zerolog/log"
 )
 
 // FN is a generic handler type
@@ -17,19 +19,68 @@ func Register(pattern string, fn FN) {
 	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
 		err := fn(w, r, cfg)
 		if err != nil {
-			log.Println(err)
+			log.Error().Err(err).Msg("Server Error")
 			w.WriteHeader(500)
 			w.Write([]byte(fmt.Sprintf("%v", err)))
 		}
 	})
 }
 
+type static struct {
+}
+
+var fs http.Handler
+
+func (s *static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.RequestURI == "/" {
+		w.Header().Add("Location", "index.go")
+		w.WriteHeader(http.StatusFound)
+	} else {
+		fs.ServeHTTP(w, r)
+	}
+}
+
 // Run launches the webserver and runs "forever"
 func Run(config *config.Config) {
 	cfg = config
-	http.Handle("/", http.FileServer(http.Dir(cfg.Static)))
+	fs = http.FileServer(http.Dir(cfg.Static))
+	s := static{}
+	http.Handle("/", &s)
 
+	enumInterfaces()
 	addr := fmt.Sprintf(":%d", cfg.WebPort)
-	log.Println("Listening to " + addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Info().Msg("Listening to " + addr)
+	http.ListenAndServe(addr, nil)
+}
+
+func enumInterfaces() (err error) {
+	ifs, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+
+	for _, iface := range ifs {
+		addrs, _ := iface.Addrs()
+		if len(addrs) == 0 {
+			continue
+		}
+
+		for _, addr := range addrs {
+			switch addr.(type) {
+			case *net.IPNet:
+				ipnet := addr.(*net.IPNet)
+				if ipnet.IP.IsLoopback() {
+					continue
+				}
+				ip4 := ipnet.IP.To4()
+				if ip4 == nil {
+					continue // Skip IPv6 addresses
+				}
+
+				log.Info().Str("interface", iface.Name).Str("URL", fmt.Sprintf("http://%s:%d/", ip4, cfg.WebPort)).Msg("Connect")
+			}
+
+		}
+	}
+	return
 }
